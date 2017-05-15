24f68ddfebc211a88d15723eb47ea9df3659e313
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-05-15 14:04:22 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Created by mb977 on 5/15/17.
- */
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(new File("alreadyOnJAMF"));
         Scanner sc2 = new Scanner(new File("checkThese"));
-        FileWriter fw = new FileWriter(new File("ATV"));
         ArrayList<String> lines = new ArrayList<>();
         ArrayList<String> alreadyOn = new ArrayList<>();
         ArrayList<String> print = new ArrayList<>();
         while (sc2.hasNextLine()) {
             lines.add(sc2.nextLine());
-
         }
         while (sc.hasNextLine()) {
             alreadyOn.add(sc.nextLine());
@@ -43,17 +38,73 @@ public class Main {
         for (int k = 0; k < print.size(); k++) {
             boolean hello = true;
             if (print.get(k).toUpperCase().contains("ATV")) {
-                fw.write(print.get(k) + "\n");
                 print.remove(k);
+
                 lines.remove(k);
                 k--;
                 hello = false;
             }
             if (hello) {
-                System.out.println(print.get(k));
+
+                /// System.out.println(print.get(k));
             }
         }
-        System.out.println(print.size());
-        fw.close();
+        System.out.println("Middle School West");
+        CharSequence west = "mw";
+        for (String x : print) {
+            if (x.contains(west)) {
+                System.out.println(x);
+            }
+        }
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("Middle School east");
+        CharSequence east = "me";
+        for (String x : print){
+            if(x.contains(east)){
+                System.out.println(x);
+            }
+        }
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("South elementary");
+        CharSequence south = "so";
+        for (String x : print){
+            if(x.contains(south)){
+                System.out.println(x);
+            }
+        }
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("Skippack elementary");
+        CharSequence skp = "sk";
+        for (String x : print){
+            if(x.contains(skp)){
+                System.out.println(x);
+            }
+        }
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("Evergreen elementary");
+        CharSequence ev = "eg";
+        for (String x : print){
+            if(x.contains(ev)){
+                System.out.println(x);
+            }
+        }
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+        System.out.println("Schwenksville elementary");
+        CharSequence sch = "sw";
+        for (String x : print){
+            if(x.contains(sch)){
+                System.out.println(x);
+            }
+        }
+        System.out.println(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
+
+        System.out.println("High School");
+        CharSequence hs = "hs";
+        for (String x : print){
+            if(x.contains(hs)){
+                System.out.println(x);
+            }
+        }
+
     }
 }
